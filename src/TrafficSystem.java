5cf815ca3ca0e1b44a0a62bea5a57d58cb058d6a
Med-Elalj <mohammedelalj@outlook.com>
2025-11-23 18:27:49 +0100

    init class

---
+++ src/TrafficSystem.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public class TrafficSystem {
+
+    public static class TrafficHub {
+
+        public TrafficHub() {
+        }
+    }
+
+    public static void checkLights(Vehicle vehicle, TrafficHub hub) {
+        return
+    }
+
+    public static void updateLights(TrafficHub hub, List<Vehicle> vehicles) {
+        return;
+    }
+}
